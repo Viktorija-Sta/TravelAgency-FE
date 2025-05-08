b6b1ae409e6368052d7e6a5d1402ad70c01c0a0d
Viktorija-Sta <viktorija.ribilyte@gmail.com>
2025-05-08 09:25:52 +0300

    fix: Update axios baseURL to point to the production API endpoint

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import { jwtDecode } from "jwt-decode"
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://atsiskaitymas-fe-3tpy.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
