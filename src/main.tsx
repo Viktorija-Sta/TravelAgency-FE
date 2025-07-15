e0f36595e9490a70ac33a01620e6de872ad77334
Viktorija-Sta <viktorija.ribilyte@gmail.com>
2025-07-15 18:04:24 +0300

    fix: Consolidate ThemeProvider and CssBaseline imports from MUI

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ThemeProvider } from '@emotion/react'
-import { CssBaseline } from '@mui/material'
+import { ThemeProvider, CssBaseline } from '@mui/material'
 import theme from './theme.ts'
-
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-    <App />
-      </ThemeProvider>
-  </StrictMode>,
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 )
