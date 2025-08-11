e1502490c58fe02680062f57312026e91e5adb24
Viktorija-Sta <viktorija.ribilyte@gmail.com>
2025-08-11 15:09:12 +0300

    fix: Revert API base URL to local development environment

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import { jwtDecode } from "jwt-decode"
 
 const api = axios.create({
-  baseURL: "https://travel-agency-be-eipr.onrender.com/api",
+  baseURL: "http://localhost:2000/api",
   headers: {
     "Content-Type": "application/json",
   },
